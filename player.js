5b398b05fff3fe041003ca57f04699be9fbf0d4e
santanaarthur <57259333+santanaarthur@users.noreply.github.com>
2021-03-30 14:20:52 -0300

    Create player.js

---
+++ player.js
@@ -0,0 +1,195 @@
+let playerBadEl = document.querySelector('#play-bad');
+let pauseBadEl = document.querySelector('#pause-bad');
+let badGirlEl = document.querySelector('#bad'); 
+
+playerBadEl.addEventListener('click', tocar);
+pauseBadEl.addEventListener('click', pausar);
+
+function tocar() { 
+  badGirlEl.play(); 
+}
+function pausar() { 
+  badGirlEl.pause(); 
+}
+
+let playerDancingEl = document.querySelector('#play-dancin');
+let pauseDancingEl = document.querySelector('#pause-dancin');
+let dancingEl = document.querySelector('#dancin'); 
+
+playerDancingEl.addEventListener('click', tocar1);
+pauseDancingEl.addEventListener('click', pausar1);
+
+function tocar1() { 
+  dancingEl.play(); 
+} 
+function pausar1() { 
+  dancingEl.pause(); 
+} 
+
+let playerGetEl = document.querySelector('#play-get');
+let pauseGetEl = document.querySelector('#pause-get');
+let getEl = document.querySelector('#get_stupid'); 
+
+getEl.addEventListener('click', tocar2);
+
+function tocar2() { 
+  getEl.play(); 
+} 
+function pausar2() { 
+  getEl.pause(); 
+} 
+let playerSudnoEl = document.querySelector('#play-sudno');
+let pauseSudnoEl = document.querySelector('#pause-sudno');
+let sudnoEl = document.querySelector('#sudno'); 
+
+playerSudnoEl.addEventListener('click', tocar3);
+pauseSudnoEl.addEventListener('click', pausar3);
+
+
+function tocar3() { 
+  sudnoEl.play(); 
+} 
+function pausar3() { 
+  sudnoEl.pause(); 
+} 
+
+let playerBadEl = document.querySelector('#play-bad');
+let pauseBadEl = document.querySelector('#pause-bad');
+let badGirlEl = document.querySelector('#bad'); 
+
+playerBadEl.addEventListener('click', tocar);
+pauseBadEl.addEventListener('click', pausar);
+
+function tocar4() { 
+  badGirlEl.play(); 
+}
+function pausar4() { 
+  badGirlEl.pause(); 
+}
+let playerBadEl = document.querySelector('#play-bad');
+let pauseBadEl = document.querySelector('#pause-bad');
+let badGirlEl = document.querySelector('#bad'); 
+
+playerBadEl.addEventListener('click', tocar);
+pauseBadEl.addEventListener('click', pausar);
+
+function tocar5() { 
+  badGirlEl.play(); 
+}
+function pausar5() { 
+  badGirlEl.pause(); 
+}
+
+let playerBadEl = document.querySelector('#play-bad');
+let pauseBadEl = document.querySelector('#pause-bad');
+let badGirlEl = document.querySelector('#bad'); 
+
+playerBadEl.addEventListener('click', tocar);
+pauseBadEl.addEventListener('click', pausar);
+
+function tocar6() { 
+  badGirlEl.play(); 
+}
+function pausar6() { 
+  badGirlEl.pause(); 
+}
+
+let playerBadEl = document.querySelector('#play-bad');
+let pauseBadEl = document.querySelector('#pause-bad');
+let badGirlEl = document.querySelector('#bad'); 
+
+playerBadEl.addEventListener('click', tocar);
+pauseBadEl.addEventListener('click', pausar);
+
+function tocar7() { 
+  badGirlEl.play(); 
+}
+function pausar7() { 
+  badGirlEl.pause(); 
+}
+
+let playerBadEl = document.querySelector('#play-chuva');
+let pauseBadEl = document.querySelector('#pause-chuva');
+let badGirlEl = document.querySelector('#chuva'); 
+
+playerBadEl.addEventListener('click', tocar);
+pauseBadEl.addEventListener('click', pausar);
+
+function tocar8() { 
+  badGirlEl.play(); 
+}
+function pausar8() { 
+  badGirlEl.pause(); 
+}
+
+let playerfalaEl = document.querySelector('#play-falador');
+let pausefalaEl = document.querySelector('#pause-falador');
+let faladorEl = document.querySelector('#falador'); 
+
+playerfalaEl.addEventListener('click', tocar9);
+pausefalaEl.addEventListener('click', pausar9);
+
+function tocar9() { 
+  faladorEl.play(); 
+}
+function pausar9() { 
+  faladorEl.pause(); 
+}
+
+let playersemEl = document.querySelector('#play-sem-ela');
+let pausesemEl = document.querySelector('#pause-sem-ela');
+let semEl = document.querySelector('#sem_ela'); 
+
+playersemEl.addEventListener('click', tocar10);
+pausesemEl.addEventListener('click', pausar10);
+
+function tocar10() { 
+  semEl.play(); 
+}
+function pausar10() { 
+  semEl.pause(); 
+}
+
+let playerSoEl = document.querySelector('#play-say-so');
+let pauseSoEl = document.querySelector('#pause-say-so');
+let soEl = document.querySelector('#say_so'); 
+
+playerBadEl.addEventListener('click', tocar);
+pauseBadEl.addEventListener('click', pausar);
+
+function tocar11() { 
+  soEl.play(); 
+}
+function pausar11() { 
+  soEl.pause(); 
+}
+
+let playerSanjiEl = document.querySelector('#play-rap-sanji');
+let pauseSanjiEl = document.querySelector('#pause-rap-sanji');
+let sanjiEl = document.querySelector('#rap_sanji'); 
+
+playerSanjiEl.addEventListener('click', tocar12);
+pauseSanjiEl.addEventListener('click', pausar12);
+
+function tocar12() { 
+  sanjiEl.play(); 
+}
+function pausar12() { 
+  sanjiEl.pause(); 
+}
+
+let playerTantoEl = document.querySelector('#play-tanto');
+let pauseTantoEl = document.querySelector('#pause-tanto');
+let tantoEl = document.querySelector('#tantotempo'); 
+
+playerTantoEl.addEventListener('click', tocar13);
+pauseTantoEl.addEventListener('click', pausar13);
+
+function tocar13() { 
+  tantoEl.play(); 
+}
+function pausar13() { 
+  tantoEl.pause(); 
+}
+
+
