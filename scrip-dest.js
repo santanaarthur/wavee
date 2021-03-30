53d5aedd044c00d92b023e6d6c947ce8d01e3935
santanaarthur <57259333+santanaarthur@users.noreply.github.com>
2021-03-30 14:21:59 -0300

    Create scrip-dest.js

---
+++ scrip-dest.js
@@ -0,0 +1,24 @@
+$('.ja1').slideUp();
+$('#mostra-jao').click(function() {
+	$('.ja1').fadeToggle();
+});
+
+$('.sant').slideUp();
+$('#mostra-sant').click(function() {
+	$('.sant').fadeToggle();
+});
+
+$('.gast').slideUp();
+$('#mostra-gast').click(function() {
+	$('.gast').fadeToggle();
+});
+
+$('.braya').slideUp();
+$('#mostra-braya').click(function() {
+	$('.braya').fadeToggle();
+});
+
+$('.marks').slideUp();
+$('#mostra-marks').click(function() {
+	$('.marks').fadeToggle();
+});
